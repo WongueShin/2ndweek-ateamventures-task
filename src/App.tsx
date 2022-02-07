89e60f0faa14e065cfde144642018246f77a1795
WongueShin <won7963kr@gmail.com>
2022-02-07 18:01:25 +0900

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from "Components/Main/index.Main";
+
+function App() {
+  return (
+    <div>
+      Ateam ventures
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main/index.Main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "Components/Main/style.Main";
+
+const Main:React.FC = () => {
+    return(
+        <S.Main>Main</S.Main>
+    )
+}
+
+export default Main
