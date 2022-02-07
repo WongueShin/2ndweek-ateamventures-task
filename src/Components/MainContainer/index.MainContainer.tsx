233e4b4c48c03d37daf0aa9865c948270957f402
jyb8509 <bestdung8509@gmail.com>
2022-02-07 19:01:33 +0900

    Add: MainContainer 폴더 추가

---
+++ src/Components/MainContainer/index.MainContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "Components/MainContainer/sytle.MainContainer";
+
+const MainContainer: React.FC = () => {
+  return <S.MainContainer>Main</S.MainContainer>;
+};
+
+export default MainContainer;
