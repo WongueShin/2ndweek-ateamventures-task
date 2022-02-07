f40b97cbcd6ecb0befac28e4f89558517df41bf2
jyb8509 <bestdung8509@gmail.com>
2022-02-07 19:47:14 +0900

    Add: MainContainer의 props 타입정의

--- src/Components/MainContainer/sytle.MainContainer.ts
+++ src/Components/MainContainer/sytle.MainContainer.ts
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const MainContainer = styled.div``;
+export const MainContainer = styled.div`
+  display: grid;
+  margin-left: 16px;
+  margin-bottom: 16px;
+`;
